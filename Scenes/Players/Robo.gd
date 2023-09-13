d9c5954c123e19846f907b40254576b612545c5d
MichaelCapriel <caprielx@gmail.com>
2023-09-12 19:13:34 -0500

    Update 5
    Run, Jump, Fall functions called in code

--- Scenes/Players/Robo.gd
+++ Scenes/Players/Robo.gd
@@ -19,11 +19,10 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.y == 0:
+		anim.play("Run")
+		# As good practice, you should replace UI actions with custom gameplay actions.
+	if velocity.y < 0:
+		anim.play("Fall")
 
 	move_and_slide()
