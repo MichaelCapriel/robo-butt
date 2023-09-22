328a308e464035b66d6f18ecdefcdd6dfdaa7e96
MichaelCapriel <caprielx@gmail.com>
2023-09-22 13:57:17 -0500

    Update 9
    Spooner now spoons any Rand item/hazard placed into spawner from "world" node

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -8,6 +8,6 @@ var selected_scene_index = 0
 
 func _on_timer_timeout():
 	random_scene.randomize()
-	selected_scene_index = random_scene.randf_range(0, scenes.size()-1)
+	selected_scene_index = random_scene.randf_range(0, scenes.size())
 	var _tmp = scenes[selected_scene_index].instantiate()
 	add_child(_tmp)
