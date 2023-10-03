fd448b1b18dc02c3783a4b06d051ee7cb53ceafe
Khaymanbarr <144856844+Khaymanbarr@users.noreply.github.com>
2023-10-03 17:26:50 -0500

    enemy added
    started intergrating enemies in the game but having issues

---
+++ Scenes/enemies.gd
@@ -0,0 +1,17 @@
+extends "ScrollMovement.gd"
+
+func _physics_process(_delta):
+	move()
+
+
+func _on_dmg_body_entered(body):
+	if body.name == "Player":
+		print("player died")
+
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
